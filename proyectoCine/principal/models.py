acec7cd716d65ead27b36e83f714c29e65c69b41
davidRomero28 <david.2hh@hotmail.com>
2023-05-26 21:07:35 +0200

    Update models.py

--- proyectoCine/principal/models.py
+++ proyectoCine/principal/models.py
@@ -3,11 +3,24 @@ from django.db import models
 # Create your models here.
 
 class Pelicula(models.Model):
-    nombre  = models.CharField(max_length=200)
-    descripcion = models.TextField(null=True)
+    titulo = models.CharField(max_length=255)
+    imagen = models.ImageField(verbose_name='foto ',upload_to='peliculas',)
+    genero = models.CharField(max_length=255)
+    duracion = models.IntegerField()
+    sinopsis = models.TextField()
+    director = models.CharField(max_length=255)
+    fechaLanzamiento = models.CharField(max_length=255)
+    clasificacionEdad = models.CharField(max_length=50)
+    valoracion = models.FloatField()
     created = models.DateTimeField(auto_now_add=True,verbose_name='Fecha de creación')
-    updated = models.DateTimeField(auto_now=True,verbose_name='Fecha de modificación') 
 
+    class Meta:
+        verbose_name='pelicula'
+        verbose_name_plural="peliculas"
+        ordering=['-created']
+    
+    def __str__(self):
+        return self.titulo
 
 # CREATE TABLE Películas (
 #   titulo VARCHAR(255),
@@ -17,5 +30,5 @@ class Pelicula(models.Model):
 #   director VARCHAR(255),
 #   fechaLanzamiento VARCHAR(255),
 #   clasificacionEdad VARCHAR(50),
-#   valoracion VARCHAR(50)
+#   valoracion float
 # );
