ca54c96231d8e362c531bcc65bb90ddb53615d99
davidRomero28 <david.2hh@hotmail.com>
2023-05-29 16:28:14 +0200

    V3 main

---
+++ proyectoCine/principal/static/principal/js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $("#searchInput").on("keyup", function () {
+        var value = $(this).val().toLowerCase();
+        $(".card").filter(function () {
+            $(this).toggle($(this).find(".card-title").text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+});
