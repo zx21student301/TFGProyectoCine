f1824d45a5fbef62f08d878181002189cf0295c1
Eky <114396484+zx21student301@users.noreply.github.com>
2023-06-13 15:23:51 +0200

    all comentarios

--- proyectoCine/principal/forms.py
+++ proyectoCine/principal/forms.py
@@ -5,9 +5,3 @@ class ButacaForm(forms.ModelForm):
     class Meta:
         model = Butaca
         exclude = ['campo_no_deseado']
-
-
-class ComentarioForm(forms.ModelForm):
-    class Meta:
-        model = Comentario
-        fields = ['puntuacion', 'comentario',"pelicula","usuario"]
